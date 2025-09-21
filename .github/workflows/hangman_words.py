074e21c2cc256544ebc48debc43fbb241f83885c
Adwait Wakade <adwaitwakade@gmail.com>
2025-09-21 22:25:34 +0530

    Create hangman_words.py

---
+++ .github/workflows/hangman_words.py
@@ -0,0 +1,13 @@
+
+word_list=["adwait","wakade","laptop","where","mobile","liquor","python","pygame","animes",
+           "banana", "circle", "garden", "father", "window", "wallet", "potato", "yellow", "spring", "rocket",
+           "wisdom", "placid", "animal", "beauty", "bottle", "camera", "danger", "effort", "friend", "guitar",
+           "honest", "island", "jungle", "kitten", "legend", "magnet", "nature", "object", "people", "quartz",
+           "rescue", "silver", "travel", "unique", "vacuum", "wealth", "xenial", "yogurt", "zephyr", "absent",
+           "border", "custom", "dancer", "energy", "fabric", "glance", "hammer", "impact", "jacket", "kidney",
+           "laptop", "market", "nation", "option", "planet", "quiver", "resist", "school", "temple", "update",
+           "vision", "wander", "xylems", "yearly", "zenith", "admire", "bridge", "charge", "debate", "effigy",
+           "famous", "gentle", "honour", "injury", "jigsaw", "kernel", "lawyer", "manual", "notice", "orange",
+           "pencil", "quaint", "refine", "safety", "tunnel", "urgent", "violet", "wonder", "xenons", "yonder",
+           "zodiac", "accent", "breeze", "casual", "dollar", "empire", "fabric", "glider", "humble", "ignite"
+           ]
